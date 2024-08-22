3865bf4a76d8386389d9f40462eca7af494b67d2
George <georg.koroleff2017@yandex.ru>
2024-08-22 22:17:33 +0300

    Add migrations from models

---
+++ migrations/create-comments.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      text: {
+        type: Sequelize.TEXT,
+      },
+      articleId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Articles',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  },
+};
