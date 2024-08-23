1518b70088d5e4517a5cdca53ba890bb4ba63872
George <georg.koroleff2017@yandex.ru>
2024-08-23 08:25:22 +0300

    add main application file

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/db');
+const articleRoutes = require('./routes/article');
+const articlesRoute = require('./routes/articles');
+const commentRoutes = require('./routes/comments');
+const analyticsRoutes = require('./routes/analytics');
+
+const app = express();
+app.use(bodyParser.json());
+
+const Article = require('./models/Article');
+const Comment = require('./models/Comment');
+
+const PORT = process.env.PORT || 3000;
+
+sequelize.sync()
+  .then(() => {
+    console.log('Синхронизация завершена');
+    app.listen(PORT, () => {
+      console.log(`Сервер запущен на порту ${PORT}`);
+    });
+  })
+  .catch(err => console.error('Ошибка синхронизации:', err));
+
+app.use('/article', articleRoutes);
+app.use('/article', commentRoutes);
+app.use('/articles', articlesRoute);
+app.use('/analytic', analyticsRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Blog API');
+});
